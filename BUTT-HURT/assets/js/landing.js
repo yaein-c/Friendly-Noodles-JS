f7e1a8ed2aa3d89561fe8dff505e10e3dcd2f348
Jake M <J.Mikulin@sms.ed.ac.uk>
2021-02-13 19:29:51 +0000

    whenButtonClicked w/button in index

--- BUTT-HURT/assets/js/landing.js
+++ BUTT-HURT/assets/js/landing.js
@@ -1,1 +1,41 @@
 // Javascript specific to the landing page etc
+
+function whenButtonClicked() {
+    function openpopups() {
+        alert("IT HAS STARTED MWUHAHAHAHA");
+        alert("YOUR BUTT IS SORE AINT IT");
+        alert("YOU SHOULD NOT HAVE BEEN SITTING FOR THIS LONG");
+        alert("LOOK AT WHAT IS HAPPENING");
+        alert("WE ARE TRYING TO HELP YOU");
+        alert("HACKING INTO YOUR COMPUTER");
+        alert("WOWWWW YOUR SOCIAL MEDIA IS FILLED WITH INFORMATION");
+        alert("MUST SAVE BUTT");
+        alert("SWEAT HUMAN SWEAT");
+        alert("WE ARE TRYING TO HELP YOU");
+    }
+    
+    setTimeout(openpopups, 10000);
+    
+    function rickroll() {
+        window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+    }
+
+    setTimeout(rickroll, 20000)
+
+    function openwindows() {
+        window.open("https://www.google.com");
+        window.open("https://www.yahoo.com");
+        window.open("https://www.netflix.com");
+        window.open("https://www.amazon.com");
+        window.open("https://www.apple.com");
+        window.open("https://www.nfl.com");
+        window.open("https://www.duolingo.com");
+        window.open("https://www.instagram.com");
+        window.open("https://www.ed.ac.uk");
+        window.open("https://youtu.be/L_jWHffIx5E?t=36");
+    }
+    
+    setTimeout(openwindows, 25000)
+    
+}
+
