a70e66ac63cff0ceea7080937d0f4889de4a6cbb
Jake M <J.Mikulin@sms.ed.ac.uk>
2021-02-13 20:01:17 +0000

    Update landing.js

--- BUTT-HURT/assets/js/landing.js
+++ BUTT-HURT/assets/js/landing.js
@@ -1,42 +1,38 @@
 // Javascript specific to the landing page etc
 
 function whenButtonClicked() {
-    function openpopups() {
-        alert("IT HAS STARTED MWUHAHAHAHA");
-        alert("YOUR BUTT IS SORE AINT IT");
-        alert("YOU SHOULD NOT HAVE BEEN SITTING FOR THIS LONG");
-        alert("YOU SHOULD STAND UP RIGHT NOW");
-        alert("STAND UP");
-        alert("WE ARE TRYING TO HELP YOU");
-        alert("HACKING INTO YOUR COMPUTER");
-        alert("YOUR SOCIAL MEDIA IS FILLED WITH INFORMATION");
-        alert("MUST SAVE THE BUTT");
-        alert("SWEAT HUMAN SWEAT");
-        alert("LET THE HACKING BEGIN");
-    }
-    
     setTimeout(openpopups, 10000);
-    
-    function rickroll() {
-        window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
-    }
-
-    setTimeout(rickroll, 20000)
-
-    function openwindows() {
-        window.open("https://www.google.com");
-        window.open("https://www.yahoo.com");
-        window.open("https://www.netflix.com");
-        window.open("https://www.amazon.com");
-        window.open("https://www.apple.com");
-        window.open("https://www.nfl.com");
-        window.open("https://www.duolingo.com");
-        window.open("https://www.instagram.com");
-        window.open("https://www.ed.ac.uk");
-        window.open("https://youtu.be/L_jWHffIx5E?t=36");
-    }
-    
-    setTimeout(openwindows, 25000)
-    
+    setTimeout(rickroll, 12000);
+    setTimeout(openwindows, 25000);
 }
 
+function openpopups() {
+    alert("IT HAS STARTED MWUHAHAHAHA");
+    alert("YOUR BUTT IS SORE AINT IT");
+    alert("YOU SHOULD NOT HAVE BEEN SITTING FOR THIS LONG");
+    alert("YOU SHOULD STAND UP RIGHT NOW");
+    alert("STAND UP");
+    alert("WE ARE TRYING TO HELP YOU");
+    alert("HACKING INTO YOUR COMPUTER");
+    alert("YOUR SOCIAL MEDIA IS FILLED WITH INFORMATION");
+    alert("MUST SAVE THE BUTT");
+    alert("SWEAT HUMAN SWEAT");
+    alert("LET THE HACKING BEGIN");
+}
+
+function rickroll() {
+    window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+}
+
+function openwindows() {
+    window.open("https://www.google.com");
+    window.open("https://www.yahoo.com");
+    window.open("https://www.netflix.com");
+    window.open("https://www.amazon.com");
+    window.open("https://www.apple.com");
+    window.open("https://www.nfl.com");
+    window.open("https://www.duolingo.com");
+    window.open("https://www.instagram.com");
+    window.open("https://www.ed.ac.uk");
+    window.open("https://youtu.be/L_jWHffIx5E?t=36");
+}
